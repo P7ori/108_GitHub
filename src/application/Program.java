ce25f064a8086da1b59ccbcc6349073eafe0b40f
P7ori <pedrosartdr@gmail.com>
2021-12-15 21:05:55 -0300

    mensagem de boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,5 +5,7 @@ public class Program
     public static void main(String[] args)
     {
        System.out.println("Good morning!");
+       System.out.println("Good afternoon!");
+       System.out.println("Good evening!");
     }
 }
